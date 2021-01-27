bce8d931c3416af6d04f6cae6dc4f7bd2ae070e6
charlie <charlie@local.local>
2021-01-27 16:34:46 +0000

    Initial work on a google transit api function

---
+++ src/get_google_data/get_google_data.py
@@ -0,0 +1,36 @@
+from src.get_google_data import database_connect
+import logging
+import requests
+import os
+import configparser
+
+config = configparser.ConfigParser()
+config.read("config.ini")
+logging.getLogger(__name__)
+
+db = database_connect.db_connect()
+
+def get_directions_data(origin: str, destination: str, mode: str, arrival_time: int) -> dict:
+    """
+    Dispatches query to google transit API
+    Args:
+        origin: Google supports many types, prefered is postal code
+        destination: Google supports many types, prefered is postal code
+        mode: "driving" or "transit"
+        arrival_time: unix timestamp for arrival time
+
+    Returns:
+        dict: Json response from the google transit API
+    """
+    api_key = os.environ("GOOGLE_API_KEY")
+    url = config["General"]["google_transit_api_endpoint"]
+    params = {
+        "origin": origin,
+        "destination": destination,
+        "mode": mode,
+        "arrival_time": arrival_time,
+        "key": api_key,
+    }
+
+    req = requests.get(url, params=params)
+    return req.json()
